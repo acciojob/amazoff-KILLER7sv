4d62fa5bcf594f53883524bdd65ad6c8e1962ca6
KILLER7sv <116873144+KILLER7sv@users.noreply.github.com>
2023-06-22 10:36:35 +0530

    added delivery time

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -5,10 +5,27 @@ public class Order {
     private String id;
     private int deliveryTime;
 
-    public Order(String id, String deliveryTime) {
 
+
+    public Order(String id, String deliveryTime) {
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        int HH = 0;
+        int MM = 0;
+        int i;
+        for(i= 0 ; i < 2 ; i++){
+            int temp = deliveryTime.charAt(i) - '0';
+            HH = HH*10 + temp;
+        }
+        for(i = 3; i< 5;i++){
+            int temp2 = deliveryTime.charAt(i)-'0';
+            MM = MM*10+temp2;
+        }
+        int time = HH*60 + MM;
+        setDeliveryTime(time);
+    }
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
     }
 
     public String getId() {
--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -42,7 +42,7 @@ public class OrderController {
     @GetMapping("/get-order-by-id/{orderId}")
     public ResponseEntity<Order> getOrderById(@PathVariable String orderId){
 
-        Order order= null;
+        Order order= new Order(orderId ,"" );
         //order should be returned with an orderId.
 
         return new ResponseEntity<>(order, HttpStatus.CREATED);
